6bb80bafff6fc5e6add90edae0be45c2f0c81fbd
nozira <0.arizon.1@gmail.com>
2019-03-18 12:42:23 +0900

    e2-3

---
+++ Exercise/Exercise2_3.java
@@ -0,0 +1,16 @@
+class Exercise2_3{
+  public static void main(String[] args) {
+    int a = 30;
+    double b = 1.59253589;
+    char c = 'あ';
+    boolean d = true;
+    String e = "GoodMorningは、おはよう";
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+
+  }
+}
