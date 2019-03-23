3529aef3386b90c51ac7cd4f421a39cf41d0d480
nozira <0.arizon.1@gmail.com>
2019-03-24 00:59:23 +0900

    ex4

---
+++ Example/Example4_2.java
@@ -0,0 +1,13 @@
+class Example4_2{
+  public static void main(String[] args) {
+    int a = 10;
+
+    if(a > 0){
+      System.out.println("aは正の数です");
+    }else if (a == 0){
+      System.out.println("aは0です");
+    }else{
+      System.out.println("aは負の数です");
+    }
+  }
+}
